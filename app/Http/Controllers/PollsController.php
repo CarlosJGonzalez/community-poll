728e36291387134bf1170ea6a46ffa2e80096894
Carlos Gonzalez <siscond@hotmail.com>
2020-06-10 14:19:28 -0400

    Initial Commit

---
+++ app/Http/Controllers/PollsController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Poll;
+use Illuminate\Http\Request;
+use Validator;
+use App\Http\Resources\Poll as PollResource;
+
+class PollsController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Poll::get(), 200);
+    }
+
+    public function show($id)
+    {
+        $poll = Poll::find($id);
+        if( is_null($poll))
+        {
+            return response()->json(null, 404);
+        }
+        $response = new PollResource(Poll::findOrfail($id), 200);
+        return response()->json($response, 200);
+    }
+
+    public function store( Request $request )
+    {
+        $rules= [
+            'title' => 'required|max:255'
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()){
+            return response()->json($validator->errors(), 400);
+        }
+
+        $poll = Poll::create($request->all());
+        return response()->json($poll, 201);
+    }
+
+    public function update (Request $request, Poll $poll)
+    {
+        $poll->update($request->all());
+        return response()->json($poll, 200);
+    }
+
+    public function delete(Request $request, Poll $poll)
+    {
+        $poll->delete();
+        return response()->json(null, 204);
+    }
+}
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,3 @@
+<h3>
+    Sorry, the page/resource you are looking for could not be found.
+</h3>
